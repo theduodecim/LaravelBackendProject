28fe8fa2fd293e7f5f707e3e2bad380f0701a84d
unknown <juansebastian.012@gmail.com>
2018-11-29 01:57:33 -0300

    ForgeSetUp

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
      */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Laravel Project'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
      */
 
-    'url' => env('APP_URL', 'https://laravelbackendproject.herokuapp.com/'),
+    'url' => env('APP_URL', 'http://laravelbackendproject.tk'),
 
     /*
     |--------------------------------------------------------------------------
